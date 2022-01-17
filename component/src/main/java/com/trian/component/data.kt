e694d77f5e73e7bf631b0089f5921fab68b056b8
trian damai <triannurizkillah@gmail.com>
2022-01-17 09:26:24 +0700

    feat: add data

---
+++ component/src/main/java/com/trian/component/data.kt
@@ -0,0 +1,28 @@
+package com.trian.component
+
+import com.trian.domain.models.FruitModel
+
+
+var dataExplore = listOf(
+    FruitModel("Banana",2000,R.drawable.banana),
+    FruitModel("Grape",2000,R.drawable.red_pepper),
+    FruitModel("Grape",2000,R.drawable.carrot),
+    FruitModel("Grape",2000,R.drawable.ginger),
+    FruitModel("Pineaple",2000,R.drawable.apple),
+)
+
+var dataBestSelling = listOf(
+    FruitModel("Banana",2000,R.drawable.banana),
+    FruitModel("Grape",2000,R.drawable.ginger),
+    FruitModel("Grape",2000,R.drawable.carrot),
+    FruitModel("Grape",2000,R.drawable.red_pepper),
+    FruitModel("Pineaple",2000,R.drawable.apple),
+)
+
+var dataExlusiveOffer = listOf(
+    FruitModel("Banana",2000,R.drawable.banana),
+    FruitModel("Grape",2000,R.drawable.red_pepper),
+    FruitModel("Pineaple",2000,R.drawable.carrot),
+    FruitModel("Pineaple",2000,R.drawable.ginger),
+    FruitModel("Pineaple",2000,R.drawable.apple),
+)
